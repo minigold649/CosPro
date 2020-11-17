88cd5af26ba903af1d894801d3a69e18fc9c74e0
akzmw <akzmw@minigold649>
2020-11-17 23:04:39 +0900

    a

--- src/minigold/Solution11.java
+++ src/minigold/Solution11.java
@@ -1,11 +1,28 @@
 package minigold;
 
+import java.util.Arrays;
+
 public class Solution11 {
 
 	public static void main(String[] args) {
+		String[] shirtSize = {"XS", "S", "L", "L", "XL", "S"};
 		
-		
-
+		Solution11 sol = new Solution11();
+		System.out.println(Arrays.toString(sol.solution(shirtSize)));
 	}
 
+	public int[] solution(String[] shirtSize) {
+		String[] standard = {"XS", "S", "M", "L", "XL", "XXL"};
+		int[] result = new int[6];
+		
+		for (int i = 0 ; i < standard.length ; i++) {
+			for (int i2 = 0 ; i2 < shirtSize.length ; i2++) {
+				if (standard[i] == shirtSize[i2]) {
+					result[i]++;
+				}
+			}
+		}
+		
+		return result;	
+	}
 }
